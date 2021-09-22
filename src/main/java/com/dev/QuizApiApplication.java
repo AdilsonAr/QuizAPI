d2bf95c808a13f8d77418a7b2cf00ab97fb46fa7
RichardAmaya26 <89000710+RichardAmaya26@users.noreply.github.com>
2021-09-22 11:38:14 -0600

    Created Richard branch

--- src/main/java/com/dev/QuizApiApplication.java
+++ src/main/java/com/dev/QuizApiApplication.java
@@ -8,6 +8,7 @@ public class QuizApiApplication {
 
 	public static void main(String[] args) {
 		//edwin was here
+		//Richard was here
 		SpringApplication.run(QuizApiApplication.class, args);
 	}
 
